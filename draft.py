70c391c0730627affeff78ae2e2765d21ec499d0
cc-yyyy <1324661239@qq.com>
2023-05-24 14:44:54 +0800

    DPPO controller for CartPole things, read debug_log.txt for details

--- draft.py
+++ draft.py
@@ -5,6 +5,7 @@ import atexit
 import time
 from itertools import product
 
+import matplotlib
 import numpy as np
 import torch
 from matplotlib import pyplot as plt
@@ -26,13 +27,4 @@ from torch.distributions import Uniform
 from environment.envs.SecondOrderIntegration.SecondOrderIntegration import SecondOrderIntegration
 
 if __name__ == '__main__':
-    l = 0.35
-    basePos = [1., 1.]
-    x = np.linspace(0, 1, 100)
-    gamma = 0.1
-    interval1 = [1 if i < gamma else 0 for i in x]
-    interval2 = [1 if i >= gamma else 0 for i in x]
-    y = ((1 - x) ** 2 - 0.8) / 2 * interval1 + (- gamma * (x - gamma / 2)) * interval2
-    # y = - x - np.tanh(2.5 * x) + 1
-    plt.plot(x, y)
-    plt.show()
+    print(matplotlib.__version__)
