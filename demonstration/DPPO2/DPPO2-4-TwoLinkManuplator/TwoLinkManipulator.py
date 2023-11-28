aa773a98c5acfa25b3bc942fd132bcefa852817d
Yang-Yefeng <18B904013@stu.hit.edu.cn>
2023-11-28 15:30:28 +0800

    Add DPPO2-4-TwoLinkManupolator (not trained)

--- demonstration/DPPO2/DPPO2-4-TwoLinkManuplator/TwoLinkManipulator.py
+++ demonstration/DPPO2/DPPO2-4-TwoLinkManuplator/TwoLinkManipulator.py
@@ -1,7 +1,4 @@
-import math
 import cv2 as cv
-import numpy as np
-
 from environment.color import Color
 from utils.functions import *
 from algorithm.rl_base import rl_base
@@ -23,7 +20,7 @@ class TwoLinkManipulator(rl_base):
 
         self.theta = theta0  # 两杆的角度，分别是杆1与y轴负半轴夹角、杆2与杆1延长线夹角，逆时针为正
         self.omega = omega0  # 两杆的角速度
-        self.t = np.zeros(2, dtype=np.float32)  # 两个转轴输入转矩
+        self.torque = np.zeros(2, dtype=np.float32)  # 两个转轴输入转矩
         self.map_size = map_size
         self.target = target
         self.midPos = self.init_midPos.copy()  # 两杆铰接位置
@@ -43,12 +40,9 @@ class TwoLinkManipulator(rl_base):
         '''hyper-parameters'''
 
         self.thetaMax = np.pi
-        self.thetaMin = - np.pi
-        self.omega1Max = np.pi
-        self.omega1Min = -np.pi
-        self.omega2Max = np.pi
-        self.omega2Min = -np.pi
-        self.tMax = 5.0
+        self.omegaMax = np.pi
+        self.eMax = 4 * self.l
+        self.torqueMax = 5.0
 
         self.miss = 0.01  # 容许误差
         self.name = 'TwoLinkManipulator'
@@ -70,12 +64,12 @@ class TwoLinkManipulator(rl_base):
                                          [-self.static_gain, self.static_gain]])
         else:
             self.state_range = np.array(
-                [[-4 * self.l, 4 * self.l],
-                 [-4 * self.l, 4 * self.l],
-                 [self.thetaMin, self.thetaMax],
-                 [self.thetaMin, self.thetaMax],
-                 [self.omega1Min, self.omega1Max],
-                 [self.omega2Min, self.omega2Max]]
+                [[-self.eMax, self.eMax],
+                 [-self.eMax, self.eMax],
+                 [-self.thetaMax, self.thetaMax],
+                 [-self.thetaMax, self.thetaMax],
+                 [-self.omegaMax, self.omegaMax],
+                 [-self.omegaMax, self.omegaMax]]
             )
         self.current_state = self.get_state()
         self.next_state = self.current_state.copy()
@@ -83,8 +77,8 @@ class TwoLinkManipulator(rl_base):
         self.action_dim = 2  # 两个转轴转矩
         self.action_step = [None, None]
         self.action_range = np.array(
-            [[-self.tMax, self.tMax],
-             [-self.tMax, self.tMax]]
+            [[-self.torqueMax, self.torqueMax],
+             [-self.torqueMax, self.torqueMax]]
         )
         self.action_num = [np.inf, np.inf]
         self.action_space = [None, None]
@@ -106,28 +100,18 @@ class TwoLinkManipulator(rl_base):
         self.image_size[0] += self.board
         self.image = np.ones([self.image_size[1], self.image_size[0], 3], np.uint8) * 255
         self.image_white = self.image.copy()  # 纯白图
-        self.base_x_pixel = 50
-        self.base_y_pixel = 20
+        self.base_x_pixel = 25
+        self.base_y_pixel = 10
         '''visualization'''
 
         self.sum_d_theta = np.zeros(2)
 
     def dis2pixel(self, coord) -> tuple:
-        """
-        :brief:         the transformation of coordinate between physical world and image
-        :param coord:   position in physical world
-        :return:        position in image coordinate
-        """
         x = self.x_offset + coord[0] * self.pixel_per_meter
         y = self.image_size[1] - self.y_offset - coord[1] * self.pixel_per_meter
         return int(x), int(y)
 
     def length2pixel(self, _l):
-        """
-        :brief:         the transformation of distance between physical world and image
-        :param _l:      length in physical world
-        :return:        length in image
-        """
         return int(_l * self.pixel_per_meter)
 
     def draw_text(self):
@@ -152,6 +136,7 @@ class TwoLinkManipulator(rl_base):
     def draw_init_image(self):
         self.draw_grid()
         self.draw_boundary()
+        self.image_white = self.image.copy()
 
     def visualization(self):
         self.image = self.image_white.copy()
@@ -242,47 +227,13 @@ class TwoLinkManipulator(rl_base):
         return False
 
     def get_reward(self, param=None):
-        # nex_s = np.concatenate((self.error, self.theta, self.omega), axis=0)
-        #
-        # nex_error = np.linalg.norm(nex_s[0: 2])
-        #
-        # nex_norm_error = nex_error / np.linalg.norm([4 * self.l, 4 * self.l])
-        #
-        # cur_norm_action = np.linalg.norm(self.t) / np.linalg.norm([self.tMax, self.tMax])
-        #
-        # if self.sum_d_theta[0] > 4 * np.pi or self.sum_d_theta[1] > 4 * np.pi:  # 如果转的超过两圈
-        #     self.terminal_flag = 4
-        #
-        # '''4. 其他'''
-        # if self.terminal_flag == 4:  # 瞎几把转
-        #     r4 = -0
-        # elif self.terminal_flag == 3:  # 成功
-        #     r4 = 1000
-        # elif self.terminal_flag == 2:  # 超时
-        #     r4 = -0
-        # elif self.terminal_flag == 1:  # 出界
-        #     r4 = -0
-        # else:
-        #     r4 = 0
-        # '''4. 其他'''
-        #
-        # '''r1 是位置'''
-        # delta = 0.1
-        # if nex_norm_error < delta:
-        #     r1 = - nex_norm_error ** 2 / 2
-        # else:
-        #     r1 = - delta * (nex_norm_error - delta / 2)
-        # '''r2 是输入'''
-        # r2 = - cur_norm_action ** 2 / 2
-        # self.reward = r1 * 10 + r4
-
         Q_pos = 2.0
         Q_omage = 0.01
         Q_acc = 0.01
 
         r_pos = -np.linalg.norm(self.error) * Q_pos
         r_omega = -np.linalg.norm(self.omega) * Q_omage
-        r_acc = -np.linalg.norm(self.t) * Q_acc
+        r_acc = -np.linalg.norm(self.torque) * Q_acc
 
         r_psi = 0.
         if self.terminal_flag == 4:  # 瞎几把转
@@ -292,33 +243,30 @@ class TwoLinkManipulator(rl_base):
 
     def ode(self, xx: np.ndarray):
         [_theta1, _theta2, _omega1, _omega2] = xx[:]
-        _dtheta1, _dtheta2 = np.clip(_omega1, self.omega1Min, self.omega1Max), np.clip(_omega2, self.omega2Min, self.omega2Max)
+        _dtheta1 = _omega1
+        _dtheta2 = _omega2
         a = np.array([[self.J * (5 + 3 * np.cos(_theta2)), self.J * (1 + 3 / 2 * np.cos(_theta2))],
-                     [self.J * (1 + 3 / 2 * np.cos(_theta2)), self.J]])
-        b = np.array([self.t[0] + 3 / 2 * self.J * np.sin(_theta2) * (_dtheta2 ** 2) + 3 * self.J * np.sin(
+                      [self.J * (1 + 3 / 2 * np.cos(_theta2)), self.J]])
+        b = np.array([self.torque[0] + 3 / 2 * self.J * np.sin(_theta2) * (_dtheta2 ** 2) + 3 * self.J * np.sin(
             _theta2) * _dtheta1 * _dtheta2 - self.m * self.g * self.l * (
                               3 / 2 * np.sin(_theta1) + 1 / 2 * np.sin(_theta1 + _theta2)),
-                      self.t[1] - 3 / 2 * self.J * np.sin(_theta2) * (
+                      self.torque[1] - 3 / 2 * self.J * np.sin(_theta2) * (
                               _dtheta1 ** 2) - 1 / 2 * self.m * self.g * self.l * np.sin(_theta1 + _theta2)])
         _domega1, _domega2 = np.linalg.solve(a, b)
         return np.array([_dtheta1, _dtheta2, _domega1, _domega2])
 
     def rk44(self, action: np.ndarray):
-        self.t = np.clip(action, -self.tMax, self.tMax)
-        h = self.dt / 1
-        tt = self.time + self.dt
-        theta = self.theta
-        while self.time < tt:
-            xx_old = np.concatenate((self.theta, self.omega), axis=0)
-            K1 = h * self.ode(xx_old)
-            K2 = h * self.ode(xx_old + K1 / 2)
-            K3 = h * self.ode(xx_old + K2 / 2)
-            K4 = h * self.ode(xx_old + K3)
-            xx_new = xx_old + (K1 + 2 * K2 + 2 * K3 + K4) / 6
-            xx_new[2] = np.clip(xx_new[2], self.omega1Min, self.omega1Max)
-            xx_new[3] = np.clip(xx_new[3], self.omega2Min, self.omega2Max)
-            self.theta, self.omega = xx_new[:2].copy(), xx_new[2:].copy()
-            self.time += h
+        self.torque = action
+        theta = self.theta.copy()
+        xx = np.concatenate((self.theta, self.omega))
+        K1 = self.dt * self.ode(xx)
+        K2 = self.dt * self.ode(xx + K1 / 2)
+        K3 = self.dt * self.ode(xx + K2 / 2)
+        K4 = self.dt * self.ode(xx + K3)
+        xx = xx + (K1 + 2 * K2 + 2 * K3 + K4) / 6
+        self.theta[:] = xx[0: 2]
+        self.omega[:] = xx[2: 4]
+        self.time += self.dt
         '''正运动学'''
         self.midPos = np.array([self.l * np.sin(self.theta[0]), -self.l * np.cos(self.theta[0])]) + self.basePos
         self.endPos = self.midPos + np.array([self.l * np.sin(sum(self.theta)), -self.l * np.cos(sum(self.theta))])
@@ -327,19 +275,24 @@ class TwoLinkManipulator(rl_base):
         '''正运动学'''
         self.error = self.target - self.endPos
         self.sum_d_theta += np.fabs(theta - self.theta)
-        self.theta[0] = self.theta[0] - 2 * self.thetaMax if self.theta[0] > self.thetaMax else \
-            self.theta[0] - 2 * self.thetaMin if self.theta[0] < self.thetaMin else self.theta[0]
-        self.theta[1] = self.theta[1] - 2 * self.thetaMax if self.theta[1] > self.thetaMax else \
-            self.theta[1] - 2 * self.thetaMin if self.theta[1] < self.thetaMin else self.theta[1]
+        if self.theta[0] > self.thetaMax:
+            self.theta[0] -= 2 * self.thetaMax
+        elif self.theta[0] < -self.thetaMax:
+            self.theta[0] += 2 * self.thetaMax
+        else:
+            pass
+
+        if self.theta[1] > self.thetaMax:
+            self.theta[1] -= 2 * self.thetaMax
+        elif self.theta[1] < -self.thetaMax:
+            self.theta[1] += 2 * self.thetaMax
+        else:
+            pass
 
     def step_update(self, action: np.ndarray):
         self.current_action = action.copy()
         self.current_state = self.get_state()
-
-        '''rk44'''
         self.rk44(action=action)
-        '''rk44'''
-
         self.is_terminal = self.is_Terminal()
         self.next_state = self.get_state()
         self.get_reward()
@@ -349,13 +302,15 @@ class TwoLinkManipulator(rl_base):
             '''通过极坐标方式在机械臂动作空间圆内生成随机目标点'''
             phi = np.random.random() * 2 * np.pi
             r = np.random.uniform(0.3 ** 2, (2 * self.l) ** 2)
-            x = math.cos(phi) * (r ** 0.5) + self.basePos[0]
-            y = math.sin(phi) * (r ** 0.5) + self.basePos[1]
+            x = np.cos(phi) * (r ** 0.5) + self.basePos[0]
+            y = np.sin(phi) * (r ** 0.5) + self.basePos[1]
             self.init_target = np.array([x, y])
+            self.init_theta = np.random.uniform(-self.thetaMax, self.thetaMax, 2)
             '''通过极坐标方式在机械臂动作空间圆内生成随机目标点'''
+
         self.theta = self.init_theta.copy()
         self.omega = self.init_omega.copy()
-        self.t = np.zeros(2)
+        self.torque = np.zeros(2)
         self.target = self.init_target.copy()
         self.midPos = self.init_midPos.copy()
         self.endPos = self.init_endPos.copy()
@@ -369,7 +324,6 @@ class TwoLinkManipulator(rl_base):
         self.current_action = np.zeros(self.action_dim)
         self.reward = 0.0
         self.is_terminal = False
-        self.terminal_flag = 0  # 0-正常 1-出界 2-超时 3-成功
+        self.terminal_flag = 0
 
         self.draw_init_image()
-
